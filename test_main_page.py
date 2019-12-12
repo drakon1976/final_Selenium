31e099ee93fd60f2504728f738009d3967d9017f
drakon1976 <39679258+drakon1976@users.noreply.github.com>
2019-12-12 11:48:59 +0200

    begining Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,9 @@
-import time
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-    time.sleep(5)
+   browser.get(link)
+   go_to_login_page(browser)
